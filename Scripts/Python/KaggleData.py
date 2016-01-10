ee43a2ceb3ac0bfa28bb3608baf626571beb88b6
gargi <michael.janschek@gmx.de>
2016-01-10 23:35:24 +0100

    1st implementaion of kNN => noteKnn

--- Scripts/Python/KaggleData.py
+++ Scripts/Python/KaggleData.py
@@ -27,6 +27,7 @@ def createCsvDictionary():
 			csvDict[row[iid]] = row
 	return csvDict,header
 
+
 """
 returns one feature of given set, making no further changes to it
 """
@@ -72,86 +73,47 @@ def getFeatureListAll():
 	'PRI_jet_subleading_eta', 'PRI_jet_subleading_phi', 'PRI_jet_all_pt', 'Weight', 'Label', 'KaggleSet', 'KaggleWeight']
 	return featureList
 
-def getFeatureSets(csvDict,header,featureName,testset = ["v"]):
-    trainFeature = getFeatureAsNpArray(
-        csvDict,header,featureName,["t"],hasErrorValues = True)
-    testFeature = getFeatureAsNpArray(
-        csvDict,header,featureName,testset,hasErrorValues = True)
+#original feature-list without
+def getFeatureListOnlyData():
+	featureList = ['EventId', 'DER_mass_MMC', 'DER_mass_transverse_met_lep', 'DER_mass_vis', 'DER_pt_h', 
+	'DER_deltaeta_jet_jet', 'DER_mass_jet_jet', 'DER_prodeta_jet_jet', 'DER_deltar_tau_lep',
+	'DER_pt_tot', 'DER_sum_pt', 'DER_pt_ratio_lep_tau', 'DER_met_phi_centrality',
+	'DER_lep_eta_centrality', 'PRI_tau_pt', 'PRI_tau_eta', 'PRI_tau_phi', 'PRI_lep_pt',
+	'PRI_lep_eta', 'PRI_lep_phi', 'PRI_met', 'PRI_met_phi', 'PRI_met_sumet', 'PRI_jet_num',
+	'PRI_jet_leading_pt', 'PRI_jet_leading_eta', 'PRI_jet_leading_phi', 'PRI_jet_subleading_pt',
+	'PRI_jet_subleading_eta', 'PRI_jet_subleading_phi', 'PRI_jet_all_pt']
+	return featureList
+
+def getFeatureSets(featureName,header,test_data,train_data):
+    index = header.index(featureName)
+    trainFeature = train_data[:,index]
+    testFeature = test_data[:,index]
     return trainFeature, testFeature
 
-def getCustomKaggleData(kaggleSets,featureList,hasErrorValues=False):
-	if featureList is None:
-		featureList = getFeatureListAll()
-	elif _checkCustomFeatureList(featureList) is False:
-		print("Error! Custom Featurelist not a subset of original Featurelist")
-		return None,None
+def getCustomDataSet(featureList,csvDict=None,header=None,kSet = "v"):
+	if csvDict is None:
+		csvDict,header = createCsvDictionary()
 
+	labels = getFeatureAsNpArray(csvDict,header,"Label",kSet,hasErrorValues=True)
+	kWeights = getFeatureAsNpArray(csvDict,header,"KaggleWeight",kSet,hasErrorValues=True)
+	data = np.zeros(shape = (len(labels),len(featureList)),dtype=float)
+	
+	for	feature in featureList:
+		i = header.index(feature)
+		data[:,i] = getFeatureAsNpArray(csvDict,header,feature,kSet,hasErrorValues=True)
 
-	csvDict,header = _createCsvDictionary()
+	return header, data, kWeights, labels
 
-	indexKaggleSet = header.index("KaggleSet")
+def getOneDataSet(csvDict=None,header=None,kSet = "v"):
+	if csvDict is None:
+		csvDict,header = createCsvDictionary()
+	featureList = getFeatureListOnlyData()
 
-	dataHeader = []
-	dataHeader = list(featureList)
+	return getCustomDataSet(featureList,csvDict,header,kSet)
 
-	if "Label" in featureList:
-		indexLabel = header.index("Label")
-		hasLabel = True
-		dataHeader.append("BinaryLabel")
-		dataHeader.append("LabelColor")
-	else:
-		hasLabel = False
-
-	dataDict={}
-	for event in csvDict:
-		if csvDict[event][indexKaggleSet] in kaggleSets:
-
-			row = []
-
-			for featureName in featureList:
-				featureID = header.index(featureName)
-				featureValue = csvDict[event][featureID]
-				row.append(featureValue)
-
-			if hasLabel is True:
-				label = csvDict[event][indexLabel]
-				if label == "s":
-					labelColor = "b" ##signal = blue
-					row.append(1)
-				elif label == "b":
-					labelColor = "r" ##background = red
-					row.append(0)
-				else:
-					print("ERROR in Labels!")
-				row.append(labelColor)
-				
-			##cutting Errors
-			if "-999.0" in row and hasErrorValues is False:
-				continue
-
-			#row-format: [feat1,feat2,...,featn (,color)]
-			dataDict[event] = row
-	print("Dictionary-Length:", len(dataDict))
-	return dataDict, dataHeader
-
-
-
-def getKaggleDataTraining(featureList = None):
-	dataDict, dataHeader = getCustomKaggleData(["t"],featureList)
-	return dataDict, dataHeader
-
-def getKaggleDataPublic(featureList = None):
-	dataDict, dataHeader = getCustomKaggleData(["b"],featureList)
-	return dataDict, dataHeader
-
-def getKaggleDataPrivate(featureList = None):
-	dataDict, dataHeader = getCustomKaggleData(["v"],featureList)
-	return dataDict, dataHeader
-
-def getKaggleDataUnused(featureList = None):
-	dataDict, dataHeader = getCustomKaggleData(["u"],featureList)
-	return dataDict, dataHeader
-
-def getKaggleDataAll(featureList = None):
-	dataDict, dataHeader = getCustomKaggleData(["t","b","v","u"],featureList)
-	return dataDict, dataHeader
+def getWholeDataSet(csvDict=None,header=None,kSet = "v"):
+	if csvDict is None:
+		csvDict,header = createCsvDictionary()
+	header, test_data, test_weights, test_labels = getOneDataSet(csvDict,header,kSet)
+	train_data, train_weights, train_labels = getOneDataSet(csvDict,header,"t")[1:4]
+	return header, test_data, test_weights, test_labels, train_data, train_weights, train_labels
